d4bea350f5736cca77a245cda6526d8b854009e5
Martico2432 <mail.joks02@gmail.com>
2024-07-01 16:49:45 +0000

    Added more specifications

--- main.py
+++ main.py
@@ -6,4 +6,17 @@ print("    Open ZIP    ")
 print("----------------")
 level = 9
 filetoget = input("Enter file name to compress: ")
+level = int(input("""
+----Compression Levels:----
+0: No compression (store only)
+1: Fastest compression (less compression, faster execution)
+2: Fast compression (balance between compression and execution speed)
+3: Fast compression (balance between compression and execution speed)
+4: Normal compression (default level, good balance between compression and execution speed)
+5: Normal compression (default level, good balance between compression and execution speed)
+6: Maximum compression (more compression, slower execution)
+7: Maximum compression (more compression, slower execution)
+8: Ultra compression (best possible compression, slowest execution)
+9: Ultra compression (best possible compression, slowest execution)
+                     """))
 compress(level=level, filename=filetoget, outputname="Outputzip.zip", filewrite="a")
